4148ed3ce591b5be9a57d5a580bd35d236df158c
leeyeonjun <114210865+leeyeonjun85@users.noreply.github.com>
2022-10-06 22:32:32 +0900

    221006-2232

--- src/script.js
+++ src/script.js
@@ -1,23 +1,16 @@
 let myImage = document.querySelector("img");
-let img1 = "https://pixabay.com/get/gb4febf5bf67b948def3d3c7b0e57bdc58840a007d1f25733632c4e72bc24e45286019b584b5d7ee314ff5acc0b8b4909_640.jpg"
-let img2 = "https://pixabay.com/get/gf82ab36fd5d0e6c003d8bd7c1b5395666982d883a43f61ecef5dd287701500fbc1be75c985188505ee5a6d64b9617b2d_640.jpg"
+let img1 =
+	"https://pixabay.com/get/gb4febf5bf67b948def3d3c7b0e57bdc58840a007d1f25733632c4e72bc24e45286019b584b5d7ee314ff5acc0b8b4909_640.jpg";
+let img2 =
+	"https://pixabay.com/get/gaae61a809bbbc5f726d8227f886310b3c9e18e6fc74111a7f865ce63e943ef52799a0766ca5b351fd6c9b084b2ea639c9b3e99880b131f320f32eead7a51527e1aa94747d677561d9648ac237ab79fa9_640.jpg";
 
 myImage.onclick = function () {
-  let mySrc = myImage.getAttribute("src");
-  if (
-    mySrc ===
-    img2
-  ) {
-    myImage.setAttribute(
-      "src",
-      img1
-    );
-  } else {
-    myImage.setAttribute(
-      "src",
-      img2
-    );
-  }
+	let mySrc = myImage.getAttribute("src");
+	if (mySrc === img2) {
+		myImage.setAttribute("src", img1);
+	} else {
+		myImage.setAttribute("src", img2);
+	}
 };
 
 let myButton = document.querySelector("button");
@@ -25,16 +18,16 @@ let myHeading = document.querySelector("h1");
 let myWelcomeMsg = document.getElementById("welcomeMsg");
 
 myButton.onclick = function () {
-  setUserName();
+	setUserName();
 };
 
 function setUserName() {
-  let myName = prompt("이름을 알려주세요.");
-  if (!myName || myName === null) {
-    // setUserName();
-    alert("이름을 입력하세요.");
-  } else {
-    localStorage.setItem("name", myName);
-    myWelcomeMsg.innerHTML = "안녕하세요, " + myName + "님!";
-  }
+	let myName = prompt("이름을 알려주세요.");
+	if (!myName || myName === null) {
+		// setUserName();
+		alert("이름을 입력하세요.");
+	} else {
+		localStorage.setItem("name", myName);
+		myWelcomeMsg.innerHTML = "안녕하세요, " + myName + "님!";
+	}
 }
